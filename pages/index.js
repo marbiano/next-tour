c6788ffc4500ab7a41b337b3ee942fb48027a595
marbiano <martin@Martins-MacBook-Pro.local>
2020-08-20 02:26:22 -0300

    Add support for SSR

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,9 @@
-import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { fetchAllProducts } from '../lib/api';
 import Sidebar from '../components/Sidebar';
 import ProductItem from '../components/ProductItem';
 
-export default function Home() {
-  const [products, setProducts] = useState();
-
-  useEffect(() => {
-    async function fetchProducts() {
-      const newProducts = await fetchAllProducts();
-      setProducts(newProducts);
-    }
-
-    fetchProducts();
-  }, []);
-
-  if (!products) {
-    return (
-      <div className="container mx-auto mt-32 text-center">Loading...</div>
-    );
-  }
-
+export default function Home({ products }) {
   if (products.length === 0) {
     return (
       <div className="container mx-auto mt-32 text-center">
@@ -30,11 +12,8 @@ export default function Home() {
     );
   }
 
-  console.log(products);
-
   return (
     <div className="container mx-auto grid grid-cols-12 gap-8">
-      {/* Content */}
       <div className="col-span-9 mb-32">
         <ul className="grid grid-cols-3 gap-8 gap-x-16 mt-32">
           {products.map((product) => (
@@ -42,8 +21,16 @@ export default function Home() {
           ))}
         </ul>
       </div>
-
       <Sidebar />
     </div>
   );
 }
+
+export const getServerSideProps = async (context) => {
+  const products = await fetchAllProducts();
+  return {
+    props: {
+      products,
+    },
+  };
+};
--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -1,36 +1,11 @@
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
 import DefaultErrorPage from 'next/error';
 import { fetchProductBySlug } from '../../lib/api';
 import ProductView from '../../components/ProductView';
 import Sidebar from '../../components/Sidebar';
 
-export default function Product() {
-  const [product, setProduct] = useState();
-  const router = useRouter();
-  const { slug } = router.query;
-
-  useEffect(() => {
-    async function fetchProduct() {
-      const newProduct = await fetchProductBySlug(
-        Array.isArray(slug) ? slug[0] : slug,
-      );
-      setProduct(newProduct);
-    }
-
-    if (slug) fetchProduct();
-  }, [slug]);
-
-  // Product is loading
+export default function Product({ product }) {
   if (!product) {
-    return (
-      <div className="container mx-auto mt-32 text-center">Loading...</div>
-    );
-  }
-
-  // Product doesn't exist
-  if (product.error === 404) {
-    return <DefaultErrorPage statusCode={product.error} />;
+    return <DefaultErrorPage statusCode={404} />;
   }
 
   return (
@@ -40,3 +15,24 @@ export default function Product() {
     </div>
   );
 }
+
+export const getServerSideProps = async ({ params }) => {
+  const { slug } = params;
+  const product = await fetchProductBySlug(
+    Array.isArray(slug) ? slug[0] : slug,
+  );
+  return {
+    props: {
+      product,
+    },
+  };
+};
+
+// export const getStaticPaths = async () => {
+//   const products = await fetchAllProducts();
+//   return {
+//     paths: products.map((product) => ({
+//       params: { slug: product.fields.slug },
+//     })),
+//   };
+// };
