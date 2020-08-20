e68a60a7eb79fb6b6795b172a933afef76f6a4d9
marbiano <martin@Martins-MacBook-Pro.local>
2020-08-20 01:44:31 -0300

    Add Link component

--- components/ProductItem/index.js
+++ components/ProductItem/index.js
@@ -1,13 +1,17 @@
+import Link from 'next/link';
+
 export default function ProductItem({ slug, title, price, imageUrl }) {
   return (
     <li>
-      <a href={`/products/${slug}`}>
-        <div className="box">
-          <img className="box-image" src={imageUrl} />
-        </div>
-        <div className="mt-4 text-gray-700">{title}</div>
-        <div>${price}</div>
-      </a>
+      <Link href={`/products/${slug}`}>
+        <a>
+          <div className="box">
+            <img className="box-image" src={imageUrl} />
+          </div>
+          <div className="mt-4 text-gray-700">{title}</div>
+          <div>${price}</div>
+        </a>
+      </Link>
     </li>
   );
 }
--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -1,15 +1,19 @@
+import Link from 'next/link';
+
 export default function Sidebar() {
   return (
     <div className="col-start-11 col-span-2 mt-16">
       <div className="sticky top-0 pt-16">
         <h1 className="text-2xl">
-          <a href="/">La Tienda</a>
+          <Link href="/">
+            <a>La Tienda</a>
+          </Link>
         </h1>
         <p className="text-l text-gray-600">Marketplace en vivo.</p>
         <div className="mt-16">
-          <a href="/publica" className="underline text-orange-600">
-            Publicá
-          </a>
+          <Link href="/publica">
+            <a className="underline text-orange-600">Publicá</a>
+          </Link>
         </div>
       </div>
     </div>
