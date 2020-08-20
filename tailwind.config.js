6051f6180ed01a4616f6d2414aff5989948f8685
marbiano <martin@Martins-MacBook-Pro.local>
2020-08-20 00:33:38 -0300

    Lean local tooling

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+};
